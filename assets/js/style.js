c10ab36d566b72b90a6f6d6bd376cfc0514aabdb
Naomi <naomilang1212@gmail.com>
2020-11-19 20:48:42 -0800

    working state API

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,27 +1,29 @@
-var search = document.getElementById("#search");
-var stateNewDeaths = document.getElementById("#stateDeaths")
+var search = document.getElementById("search");
+var stateNewDeaths = document.querySelector("#stateDeaths")
+var stateNewCases = document.querySelector("#new-cases")
+var stateTotalCases = document.querySelector("#total-state-cases")
+var stateTotalDeaths = document.querySelector("#total-state-deaths")
 
 var searchCovid = function (){
     var searchInput = document.querySelector('#search').value;
     fetch (
         'https://api.covidtracking.com/v1/states/current.json'
+        // add + searchInput + "states/current.json"
     )
     .then (function(response){
         return response.json();
     })
     .then (function(response){
-        // if (response.ok){
-        //     addNewDeaths(response);
-        // }
         console.log(response);
-        var todaysDeaths= response[5].deathIncrease;
-        console.log(todaysDeaths);
+        var todaysDeaths = response[5].deathIncrease;
         stateNewDeaths.append(todaysDeaths);
+        var todaysNew = response[5].positiveIncrease;
+        stateNewCases.append(todaysNew);
+        var totalStateCases = response[5].positive;
+        stateTotalCases.append(totalStateCases);
+        var totalStateDeaths = response[5].deathConfirmed; 
+        stateTotalDeaths.append(totalStateDeaths);
     })
 
 }
 searchCovid();
-
-// var addNewDeaths = function(response){
-//     stateNewDeaths.append(todaysDeaths);
-// }
